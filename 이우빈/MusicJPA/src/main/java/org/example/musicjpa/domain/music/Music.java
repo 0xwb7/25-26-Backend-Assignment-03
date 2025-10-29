8426b2af5874626050c8cf679aac4dbabc7c259c
0xwb7 <woobin200111@gmail.com>
2025-10-30 01:43:33 +0900

    feat: 가수, 음악 도메인 구현

---
+++ 이우빈/MusicJPA/src/main/java/org/example/musicjpa/domain/music/Music.java
@@ -0,0 +1,40 @@
+package org.example.musicjpa.domain.music;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.musicjpa.domain.singer.Singer;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Music {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "singer_id")
+    private Singer singer;
+
+    @Builder
+    public Music(String title, Singer singer) {
+        this.title = title;
+        this.singer = singer;
+    }
+
+    public void updateMusic(String title, Singer singer) {
+        this.title = title;
+        this.singer = singer;
+    }
+}
---
+++ 이우빈/MusicJPA/src/main/java/org/example/musicjpa/domain/singer/Singer.java
@@ -0,0 +1,40 @@
+package org.example.musicjpa.domain.singer;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.musicjpa.domain.music.Music;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Singer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int debut_year;
+
+    @OneToMany(mappedBy = "singer", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Music> musics = new ArrayList<>();
+
+    @Builder
+    public Singer(String name, int debut_year) {
+        this.name = name;
+        this.debut_year = debut_year;
+    }
+}
